db9b93e409aafd85c9a650ff061c3f749f187f63
nini2 <domodanke@yahoo.com>
2011-01-02 15:34:05 +0900

    first app

---
+++ myApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyApp::Application.config.secret_token = 'f9e931fa33ed90428a15ba931a0c4243838cc1578734deb0897e13056f4b6331ce262f6e76293f18491cc45e8fbbf2b17ef0ee545d10d0b5bf4991c0ce4f61ad'
---
+++ rubygems-1.4.1/notetaker/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notetaker::Application.config.secret_token = 'b0f5767fabc8aeaaa1919a1dff203ba5fe04bc768191a8ba940f836aa1d646a24e3e4fc62cb90dab8df60f0bc4d330bf0549766bfdf3a5527f59179ca798e8ba'
